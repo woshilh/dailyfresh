d146496c094de46568a61710dd107e0e232253ad
woshilh <450406123@qq.com>
2018-02-23 21:47:43 +0800

    register

---
+++ df_user/views.py
@@ -0,0 +1,30 @@
+#coding=utf-8
+
+from django.shortcuts import render,redirect
+from models import *
+from hashlib import sha1
+
+# Create your views here.
+def register(request):
+    return render(request,'df_user/register.html')
+
+def register_handle(request):
+    post = request.POST
+    uname = post.get('user_name')
+    upwd = post.get('pwd')
+    upwd2 = post.get('cpwd')
+    uemail = post.get('email')
+    if upwd != upwd2:
+        return redirect('user/register/')
+
+    s1 = sha1()
+    s1.update(upwd)
+    upwd3 = s1.hexdigest()
+
+    user = UserInfo()
+    user.uname = uname
+    user.upwd = upwd3
+    user.uemail = uemail
+    user.save()
+    return redirect('/user/login/')
+
